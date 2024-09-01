2c5d52f35c6dc4330288dd6509698c212335c6fa
hotenabc <hotenabc789@gmail.com>
2024-09-01 09:36:11 +0700

    Update Interface.php

--- Interface.php
+++ Interface.php
@@ -1,1 +1,37 @@
+// Interface
+interface Vehicle {
+    public function start();
+    public function stop();
+}
+
+// Class implementing the interface
+class Car implements Vehicle {
+    public function start() {
+        echo "Car started." . PHP_EOL;
+    }
+    
+    public function stop() {
+        echo "Car stopped." . PHP_EOL;
+    }
+}
+
+// Class implementing the interface
+class Motorcycle implements Vehicle {
+    public function start() {
+        echo "Motorcycle started." . PHP_EOL;
+    }
+    
+    public function stop() {
+        echo "Motorcycle stopped." . PHP_EOL;
+    }
+}
+
+// Usage
+$car = new Car();
+$car->start(); // Output: Car started.
+$car->stop(); // Output: Car stopped.
+
+$motorcycle = new Motorcycle();
+$motorcycle->start(); 
+$motorcycle->stop(); 
 
